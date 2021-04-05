ee8564014505e863716856d3f067540d3552cc9b
Jaka Kump <jakakump123@gmail.com>
2021-04-05 18:16:48 +0200

    Fnkcije v classu igra

--- Igra_Gomoku/src/Logika/Igra.java
+++ Igra_Gomoku/src/Logika/Igra.java
@@ -9,18 +9,42 @@ public class Igra {
 
 	
 	public Igra() {
+		
 		plosca = new Polje[Velikost][Velikost];
 		for (int i = 0; i < Velikost; i++) {
 			for (int j = 0; j < Velikost; j++) {
 				plosca[i][j] = Polje.PRAZNO;
 			}
 		}
+		
 		naPotezi = Igralec.O;
+		
 	}
 	
 	
 	public Stanje stanje() { 
 	// kakšno je stanje igre
+		
+		return Stanje.V_TEKU;
+		
+	}
+	
+	
+	
+	public void naslednji() {
+		// spremeni igralca, ki je napotezi
+		if (this.naPotezi.equals(Igralec.X)) {
+			this.naPotezi = Igralec.O;			
+		} else {
+			this.naPotezi = Igralec.X;	
+		}
+		
+	}
+	
+	
+	
+	public static void poteza(int vrsta, int stolpec) {
+		// odigra potezo
 		
 	}
 	
