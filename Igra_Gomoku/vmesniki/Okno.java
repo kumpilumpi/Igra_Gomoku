91f65833dbf8eff73e7db674686d85a47ab938d0
teo <teo@sandim>
2021-05-08 22:40:55 +0200

    Dodan meni inteligenca s submeniji in opcija za ustavitev igre
    opcija za ustavitev dodana da lahko menjavamo vrsto algoritma od igre do
    igre

--- Igra_Gomoku/vmesniki/Okno.java
+++ Igra_Gomoku/vmesniki/Okno.java
@@ -33,7 +33,12 @@ public class Okno extends JFrame implements ActionListener {
 	private JMenuItem igraClovekClovek;
 	private JMenuItem igraRacunalnikRacunalnik;
 	private JMenuItem potezaRazveljavi;
-
+	private JMenuItem beliRand;
+	private JMenuItem crniRand;
+	private JMenuItem beliMinimax;
+	private JMenuItem crniMinimax;
+	private JMenuItem suspend;
+	
 	/**
 	 * Ustvari novo glavno okno in prični igrati igro.
 	 */
@@ -50,6 +55,8 @@ public class Okno extends JFrame implements ActionListener {
 		menu_bar.add(igra_menu);
 		JMenu poteza_menu = new JMenu("Edit");
 		menu_bar.add(poteza_menu);
+		JMenu intel_menu = new JMenu("Inteligenca");
+		menu_bar.add(intel_menu);
 
 		igraClovekRacunalnik = new JMenuItem("Človek – računalnik");
 		igra_menu.add(igraClovekRacunalnik);
@@ -67,9 +74,35 @@ public class Okno extends JFrame implements ActionListener {
 		igra_menu.add(igraRacunalnikRacunalnik);
 		igraRacunalnikRacunalnik.addActionListener(this);
 		
+		igra_menu.addSeparator();
+		suspend = new JMenuItem("Suspend");
+		igra_menu.add(suspend);
+		suspend.addActionListener(this);
+		
 		potezaRazveljavi = new JMenuItem("Razveljavi");
 		poteza_menu.add(potezaRazveljavi);
 		potezaRazveljavi.addActionListener(this);
+		
+		JMenu beli_menu = new JMenu("Beli Igralec");
+		intel_menu.add(beli_menu);
+		JMenu crni_menu = new JMenu("Črni Igralec");
+		intel_menu.add(crni_menu);		
+
+		beliRand = new JMenuItem("Naključno");
+		beli_menu.add(beliRand);
+		beliRand.addActionListener(this);
+		
+		crniRand = new JMenuItem("Naključno");
+		crni_menu.add(crniRand);
+		crniRand.addActionListener(this);
+		
+		beliMinimax = new JMenuItem("Minimax");
+		beli_menu.add(beliMinimax);
+		beliMinimax.addActionListener(this);
+		
+		crniMinimax = new JMenuItem("Minimax");
+		crni_menu.add(crniMinimax);
+		crniMinimax.addActionListener(this);
 		
 		// igralno  polje
 		platno = new Platno();
@@ -133,6 +166,31 @@ public class Okno extends JFrame implements ActionListener {
 			Vodja.vrstaIgralca.put(Igralec.X, VrstaIgralca.R);
 			Vodja.igramoNovoIgro();
 		}
+		
+		else if (e.getSource() == suspend) {
+			Vodja.igra = null;
+			osvezi();
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == beliRand) {
+			status.setText("Beli igralec igra naključno.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == crniRand) {
+			status.setText("Črni igralec igra naključno.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == beliMinimax) {
+			status.setText("Beli igralec igra z minimax.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == crniMinimax) {
+			status.setText("Črni igralec igra z minimax.");
+		}
+		
+		
+		
+		
 	}
 	
 	public void osvezi() {
