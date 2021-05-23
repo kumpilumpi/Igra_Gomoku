3c04b9d435fae72d72a0d27158be89f1b0a54083
kumpilumpi <49913061+kumpilumpi@users.noreply.github.com>
2021-05-23 11:28:08 +0200

    Update Okno.java
    Prekopirano teo-gui

--- Igra_Gomoku/vmesniki/Okno.java
+++ Igra_Gomoku/vmesniki/Okno.java
@@ -10,9 +10,6 @@ import java.util.EnumMap;
 
 import javax.swing.*;
 
-import inteligenca.Inteligenca;
-import inteligenca.Minimax;
-import inteligenca.OceniPozicijo;
 import vodja.Vodja;
 import vodja.VrstaIgralca;
 import logika.Igralec;
@@ -36,12 +33,12 @@ public class Okno extends JFrame implements ActionListener {
 	private JMenuItem igraClovekClovek;
 	private JMenuItem igraRacunalnikRacunalnik;
 	private JMenuItem potezaRazveljavi;
+	private JMenuItem beliRand;
+	private JMenuItem crniRand;
+	private JMenuItem beliMinimax;
+	private JMenuItem crniMinimax;
+	private JMenuItem suspend;
 	
-	//Preverjanje
-	private JButton ocena;
-	public static Inteligenca minimax = new Minimax(1);
-	
-
 	/**
 	 * Ustvari novo glavno okno in prični igrati igro.
 	 */
@@ -58,6 +55,8 @@ public class Okno extends JFrame implements ActionListener {
 		menu_bar.add(igra_menu);
 		JMenu poteza_menu = new JMenu("Edit");
 		menu_bar.add(poteza_menu);
+		JMenu intel_menu = new JMenu("Inteligenca");
+		menu_bar.add(intel_menu);
 
 		igraClovekRacunalnik = new JMenuItem("Človek – računalnik");
 		igra_menu.add(igraClovekRacunalnik);
@@ -75,13 +74,35 @@ public class Okno extends JFrame implements ActionListener {
 		igra_menu.add(igraRacunalnikRacunalnik);
 		igraRacunalnikRacunalnik.addActionListener(this);
 		
+		igra_menu.addSeparator();
+		suspend = new JMenuItem("Suspend");
+		igra_menu.add(suspend);
+		suspend.addActionListener(this);
+		
 		potezaRazveljavi = new JMenuItem("Razveljavi");
 		poteza_menu.add(potezaRazveljavi);
 		potezaRazveljavi.addActionListener(this);
 		
-		ocena = new JButton("Ocena");
-		menu_bar.add(ocena);
-		ocena.addActionListener(this);
+		JMenu beli_menu = new JMenu("Beli Igralec");
+		intel_menu.add(beli_menu);
+		JMenu crni_menu = new JMenu("Črni Igralec");
+		intel_menu.add(crni_menu);		
+
+		beliRand = new JMenuItem("Naključno");
+		beli_menu.add(beliRand);
+		beliRand.addActionListener(this);
+		
+		crniRand = new JMenuItem("Naključno");
+		crni_menu.add(crniRand);
+		crniRand.addActionListener(this);
+		
+		beliMinimax = new JMenuItem("Minimax");
+		beli_menu.add(beliMinimax);
+		beliMinimax.addActionListener(this);
+		
+		crniMinimax = new JMenuItem("Minimax");
+		crni_menu.add(crniMinimax);
+		crniMinimax.addActionListener(this);
 		
 		// igralno  polje
 		platno = new Platno();
@@ -121,22 +142,6 @@ public class Okno extends JFrame implements ActionListener {
 			}
  			Toolkit.getDefaultToolkit().beep();
 		}
-		
-		//PREVERJANJE
-		
-		else if (e.getSource() == ocena) {
-//			minimax.izberiPotezo(Vodja.igra);
-			
-			
-			System.out.println(minimax.izberiPotezo(Vodja.igra));
-			
-			int ocena = OceniPozicijo.oceniPozicijo1(Vodja.igra, Vodja.igra.naPotezi);
-
-			System.out.println(ocena);
-		}
-		
-		//
-		
 		else if (e.getSource() == igraClovekClovek) {
 			Vodja.vrstaIgralca = new EnumMap<Igralec,VrstaIgralca>(Igralec.class);
 			Vodja.vrstaIgralca.put(Igralec.O, VrstaIgralca.C); 
@@ -161,6 +166,31 @@ public class Okno extends JFrame implements ActionListener {
 			Vodja.vrstaIgralca.put(Igralec.X, VrstaIgralca.R);
 			Vodja.igramoNovoIgro();
 		}
+		
+		else if (e.getSource() == suspend) {
+			Vodja.igra = null;
+			osvezi();
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == beliRand) {
+			status.setText("Beli igralec igra naključno.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == crniRand) {
+			status.setText("Črni igralec igra naključno.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == beliMinimax) {
+			status.setText("Beli igralec igra z minimax.");
+		}
+		
+		else if (Vodja.igra == null && e.getSource() == crniMinimax) {
+			status.setText("Črni igralec igra z minimax.");
+		}
+		
+		
+		
+		
 	}
 	
 	public void osvezi() {
